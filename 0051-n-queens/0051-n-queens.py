488b14c7a240c068b25253d6e30d0c9469663036
TankChirag-1212 <106546348+TankChirag-1212@users.noreply.github.com>
2023-01-30 22:20:57 +0530

    Time: 59 ms (73.06%), Space: 14.2 MB (95.18%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.py
@@ -0,0 +1,30 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        board = [["...."] for _ in range(n)]
+        ans = []
+        col = set()
+        rightDiago = set()
+        LeftDiago = set()
+        
+        def backTrack(r,temp):
+            if r == n:
+                ans.append(temp)
+                return
+
+            for c in range(n):
+                if c in col or (r+c) in rightDiago or (r-c) in LeftDiago:
+                    continue
+                col.add(c)
+                rightDiago.add(r+c)
+                LeftDiago.add(r-c)
+                board[r] = ["." * c + "Q" + "." * (n-c-1)]
+                
+                backTrack(r+1, temp + board[r])
+
+                col.remove(c)
+                rightDiago.remove(r+c)
+                LeftDiago.remove(r-c)
+                board[r] = ["...."]
+                
+        backTrack(0,[])
+        return(ans)
