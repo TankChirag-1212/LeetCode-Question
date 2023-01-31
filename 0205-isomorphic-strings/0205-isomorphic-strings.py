f2081ce9fff8a90a1bdf08887ea09aa2230c6a9c
TankChirag-1212 <106546348+TankChirag-1212@users.noreply.github.com>
2023-01-31 13:57:04 +0530

    Time: 48 ms (54.70%), Space: 14.6 MB (15.23%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        arr1 = []
+        arr2 = []
+        for i in s:
+            arr1.append(s.index(i))
+        for j in t:
+            arr2.append(t.index(j))
+        if arr1 == arr2:
+            return True
+        else:
+            return False
