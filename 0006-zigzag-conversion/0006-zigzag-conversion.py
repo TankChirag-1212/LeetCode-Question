fff2417d24587b9c2c1e3ef0093cc827d5c712c9
TankChirag-1212 <106546348+TankChirag-1212@users.noreply.github.com>
2023-02-03 22:10:26 +0530

    Time: 914 ms (7.79%), Space: 20.9 MB (8.80%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -0,0 +1,42 @@
+
+import math
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if len(s) == 1 or numRows == 1:
+            return s
+
+        sections = len(s) // (2*numRows - 2)
+        numCols = sections*(numRows -1) + (numRows - 2)
+        if numRows == 2:
+            numCols = (len(s)// 2) + 1 
+        
+        matrix = [["0"]*numCols for _ in range(numRows)]
+        currRow = 0
+        currCol = 0
+        currIndexOfs = 0
+
+        while currIndexOfs < len(s):
+            while (currRow < numRows and currIndexOfs < len(s)):
+                matrix[currRow][currCol] = s[currIndexOfs]
+                currRow += 1
+                currIndexOfs += 1
+            
+            currRow -= 2
+            currCol += 1
+
+            # while (currRow > 0 and currIndexOfs < len(s)):
+            while (currRow > 0 and currCol < numCols and currIndexOfs < len(s)):
+                matrix[currRow][currCol] = s[currIndexOfs]
+                currRow -= 1
+                currCol += 1
+                currIndexOfs += 1
+
+        # print(f"{matrix} \n")
+        answer = ""
+
+        for row in range(len(matrix)):
+            for i in range(len(matrix[row])):
+                if matrix[row][i] != "0":
+                    answer += matrix[row][i]
+        return answer
+
