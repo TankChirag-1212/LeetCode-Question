9b9e93fdc1cbd99f96890af4f03ee233e82ef73d
TankChirag-1212 <106546348+TankChirag-1212@users.noreply.github.com>
2023-02-13 13:27:44 +0530

    Added another solution
    Added extra one solution which is most optimal

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.py
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.py
@@ -10,3 +10,10 @@ class Solution:
         count = (count + 1)//2
         
         return count
+    
+    
+    # ANOTHER SOLUTION (ONE LINER)
+    class Solution:
+    def countOdds(self, low: int, high: int) -> int:
+        count = (high - low)//2 + (high%2 or low % 2)    # (high % 2 or low % 2) this gives the value 1 if either one of low or high is odd, it gives 0 if both the low and high is even
+        return count
