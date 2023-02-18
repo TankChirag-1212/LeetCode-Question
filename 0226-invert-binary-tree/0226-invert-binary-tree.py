219939985bf632b89d80f91c36720ede5648eaa7
TankChirag-1212 <106546348+TankChirag-1212@users.noreply.github.com>
2023-02-18 10:19:01 +0530

    Time: 26 ms (94.89%), Space: 13.8 MB (94.98%) - LeetHub

---
+++ 0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        q = deque([root])
+        while q:
+            node = q.popleft()
+            if node is None:
+                break
+            temp = node.right
+            node.right = node.left
+            node.left = temp
+            if node.right:
+                q.append(node.right)
+            if node.left:
+                q.append(node.left)
+        return root
+    
+# first we created queue to store the root node in it then we pop root and saved into node
+# then we use the temp node and swap the right and left child of the current node
+# after doing that we update the new right and left child in the root and append them in the queue
+# while loop stops when all the nodes are traversed which means the queue is none/empty
+# this approach is bfs approach in reverse traversing
