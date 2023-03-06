223fe03400b529f579e2e99724dbc119a3b08eca
TankChirag-1212 <106546348+TankChirag-1212@users.noreply.github.com>
2023-03-06 09:10:36 +0530

    Time: 58 ms (48.01%), Space: 14 MB (80.43%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.py
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.py
@@ -1,11 +1,18 @@
 class Solution:
     def findKthPositive(self, arr: List[int], k: int) -> int:
-        numbers = []
+        l, r = 0, len(arr)
         
-        for i in range(1,2001):
-            numbers.append(i)
+        while l<r:
+            mid = (l + r)//2
+            if (arr[mid] -1- mid)< k:
+                l = mid+1
+            else:
+                r = mid
             
-        for i in range(len(arr)):
-            numbers.remove(arr[i])
-        print(numbers)   
-        return numbers[k-1]
+        return l+k
+    
+# we used binary searching method to solve this problem
+# we check which side of subarray haves missing value greater than k if missing value is less in left side then increamen the left pointer
+# we check the missing values using "arr[mid] - mid" where mid is the index of middle
+# we continue untill the left pointer is greater than right pointer
+# at last we return the answer as left pointers index + k
